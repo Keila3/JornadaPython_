6c4ad90aab3b6f1e740db74d4a25b2e5860fd2a7
Keila <kbkeilabarbosa@gmail.com>
2025-09-16 09:58:59 -0300

    Passo 1: entrar no sitema da empres(concluido)

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+import pyautogui
+import time
 
+pyautogui.PAUSE= 3
+
+# Passo 1: Entrar no sitema da empresa - https://dlp.hashtagtreinamentos.com/python/intensivao/login
+ # Abrir o chrome
+pyautogui.press('win')
+pyautogui.write('chrome')
+pyautogui.press('enter')
+
+time.sleep(3) #tempo extra para garantir que o chrome abriu
+
+ # Digiar o site
+pyautogui.hotkey('ctrl', 'l')
+pyautogui.write('https://dlp.hashtagtreinamentos.com/python/intensivao/login')
+pyautogui.press('enter')
+
+# Passo 2: Fazer login
+# Passo 3: Importa a base de dados
+# Passo 4: Cadastrar 1 produto
+# Passo 5: Repetir para todos os produtos
